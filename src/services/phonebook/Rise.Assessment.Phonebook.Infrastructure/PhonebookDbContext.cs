49c1aa0485766710a8dfdc8bd417c4a8abf39516
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2021-10-13 17:43:28 +0300

    DbContext created.

---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Infrastructure/PhonebookDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rise.Assessment.Phonebook.Domain.PhonebookAggregate;
+
+namespace Rise.Assessment.Phonebook.Infrastructure
+{
+    public class PhonebookDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "phonebook";
+
+        public PhonebookDbContext(DbContextOptions<PhonebookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<PersonDetail> PersonDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person", DEFAULT_SCHEMA);
+            modelBuilder.Entity<PersonDetail>().ToTable("PersonDetails", DEFAULT_SCHEMA);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
