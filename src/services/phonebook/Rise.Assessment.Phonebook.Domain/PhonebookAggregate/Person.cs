5ec0aad714b20e0a027b94393db0bbb86b66464e
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2021-10-13 17:38:03 +0300

    Phonebook aggregate models.

---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Domain/PhonebookAggregate/Person.cs
@@ -0,0 +1,34 @@
+﻿using Rise.Assessment.Phonebook.Domain.Core;
+using System.Collections.Generic;
+
+namespace Rise.Assessment.Phonebook.Domain.PhonebookAggregate
+{
+    public class Person : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Company { get; private set; }
+        
+
+        private readonly List<PersonDetail> _personDetails;
+        public IReadOnlyCollection<PersonDetail> PersonDetails => _personDetails;
+
+        public Person()
+        {
+        }
+
+        public Person(string name, string surname, string company)
+        {
+            _personDetails = new List<PersonDetail>();
+            Name = name;
+            Surname = surname;
+            Company = company;
+        }
+
+        public void AddPersonDetail(string phoneNumber, string mailAddress, string location)
+        {
+            PersonDetail added = new PersonDetail(phoneNumber, mailAddress, location);
+            _personDetails.Add(added);
+        }
+    }
+}
---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Domain/PhonebookAggregate/PersonDetail.cs
@@ -0,0 +1,29 @@
+﻿using Rise.Assessment.Phonebook.Domain.Core;
+
+namespace Rise.Assessment.Phonebook.Domain.PhonebookAggregate
+{
+    public class PersonDetail : Entity
+    {
+        public PersonDetail()
+        {
+        }
+
+        public PersonDetail(string phoneNumber, string mailAddress, string location)
+        {
+            PhoneNumber = phoneNumber;
+            MailAddress = mailAddress;
+            Location = location;
+        }
+
+        public string PhoneNumber { get; private set; }
+        public string MailAddress { get; private set; }
+        public string Location { get; private set; }
+
+        public void UpdatePersonDetail(string phoneNumber, string mailAddress, string location)
+        {
+            this.PhoneNumber = phoneNumber;
+            this.MailAddress = mailAddress;
+            this.Location = location;
+        }
+    }
+}
