e0080b729049733175cc345c1aabc62e47a73747
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2021-10-13 22:37:01 +0300

    Phonebook ReportDb configuration.

---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Infrastructure/ReportDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rise.Assessment.Phonebook.Domain.ReportAggregate;
+
+namespace Rise.Assessment.Phonebook.Infrastructure
+{
+    public class ReportDbContext : DbContext
+    {
+        public ReportDbContext(DbContextOptions<ReportDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Report> Report { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Report>().ToTable("Report");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
