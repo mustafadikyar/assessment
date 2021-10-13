950a813d0cc93428d399ed238a60641936f6024c
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2021-10-13 19:10:12 +0300

    Database initial.

---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Infrastructure/Migrations/20211013160821_initial.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rise.Assessment.Phonebook.Infrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "phonebook");
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                schema: "phonebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonDetails",
+                schema: "phonebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonDetails_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalSchema: "phonebook",
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonDetails_PersonId",
+                schema: "phonebook",
+                table: "PersonDetails",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonDetails",
+                schema: "phonebook");
+
+            migrationBuilder.DropTable(
+                name: "Person",
+                schema: "phonebook");
+        }
+    }
+}
