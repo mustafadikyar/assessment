136cfb45a40bf48ac255a5fac994a906bbcbce9b
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2021-10-13 17:49:05 +0300

    Object Mappings.

---
+++ src/services/phonebook/Rise.Assessment.Phonebook.Application/Mappings/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Rise.Assessment.Phonebook.Application.DTOs;
+using Rise.Assessment.Phonebook.Domain.PhonebookAggregate;
+
+namespace Rise.Assessment.Phonebook.Application.Mappings
+{
+    internal class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Person, PersonDTO>().ReverseMap();
+            CreateMap<PersonDetail, PersonDetailDTO>().ReverseMap();
+        }
+    }
+}
